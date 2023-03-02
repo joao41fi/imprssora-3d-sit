8953154d138b61f197c4de2b2f1b8c5f19ef2b5d
joao41fi <97768350+joao41fi@users.noreply.github.com>
2023-03-02 23:30:24 +0000

    Update text..py

--- text..py
+++ text..py
@@ -1,14 +1,23 @@
 import os
 import time
 
-# Abre o Pronsole em um terminal separado
-os.system("pronsole")
+# Inicia o Pronsole
+os.system("pronsole -n -p /dev/ttyACM0 -b 250000 &")
 
-# Aguarda alguns segundos para o Pronsole inicializar
-time.sleep(10)
+# Aguarda alguns segundos para o Pronsole iniciar
+time.sleep(5)
 
-# Envia um comando para o Pronsole
-os.system("echo connect /dev/ttyACM0 250000")
-os.system("echo G28")
+# Envia um comando G-code para a impressora 3D
+os.system("echo G28 > /tmp/pipe_pronsole")
 
-# Lê a resposta do Pronsole
+# Aguarda a impressora 3D terminar de executar o comando
+time.sleep(2)
+
+# Envia outro comando G-code para a impressora 3D
+os.system("echo G1 X10 Y10 Z0.3 F1000 > /tmp/pipe_pronsole")
+
+# Aguarda a impressora 3D terminar de executar o comando
+time.sleep(2)
+
+# Fecha o Pronsole
+os.system("echo quit > /tmp/pipe_pronsole")
