c487f38936d3e595933863acc68350c4a4718aab
joao41fi <97768350+joao41fi@users.noreply.github.com>
2023-03-02 23:21:58 +0000

    Update text..py

--- text..py
+++ text..py
@@ -8,8 +8,8 @@ os.system("lxterminal -e pronsole &")
 time.sleep(20)
 
 # Envia um comando para o Pronsole
-os.system("echo connect/dev/ttyACM0 250000 > /tmp/pipe_pronsole")
-os.system("echo G28 > /tmp/pipe_pronsole")
+os.system("connect/dev/ttyACM0 250000 > /tmp/pipe_pronsole")
+os.system("G28 > /tmp/pipe_pronsole")
 
 # Lê a resposta do Pronsole
 with open('/tmp/pipe_pronsole', 'r') as f:
