8c301a93a7e805a80a499c034a598c422ff8b1ad
joao41fi <97768350+joao41fi@users.noreply.github.com>
2023-03-02 23:16:53 +0000

    Update text..py

--- text..py
+++ text..py
@@ -2,13 +2,13 @@ import os
 import time
 
 # Abre o Pronsole em um terminal separado
-os.system("x-terminal-emulator -e pronsole")
+os.system("gnome-terminal --command='bash -c \"pronsole\"' &")
 
 # Aguarda alguns segundos para o Pronsole inicializar
 time.sleep(2)
 
 # Envia um comando para o Pronsole
-os.system("echo connect/dev/ttyACM0 250000 > /tmp/pipe_pronsole")
+os.system("echo G28 > /tmp/pipe_pronsole")
 
 # Lê a resposta do Pronsole
 with open('/tmp/pipe_pronsole', 'r') as f:
@@ -16,4 +16,4 @@ with open('/tmp/pipe_pronsole', 'r') as f:
 print(output)
 
 # Fecha o terminal do Pronsole
-os.system("killall x-terminal-emulator")
+os.system("pkill -f gnome-terminal")
