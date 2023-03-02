57b81a796e2e482216e53d1be2a9e460b720ba77
joao41fi <97768350+joao41fi@users.noreply.github.com>
2023-03-02 22:25:35 +0000

    Update app.py

--- main/app.py
+++ main/app.py
@@ -15,8 +15,8 @@ def Ligar_Impresora():
 		if request.form['checkbox_funcao'] == 'true':
 			print('primeira')
 			# Execute sua função aqui
-			os.system("pronsole")
-			os.system("connect/dev/ttyACM0 250000")
+			os.system("pronsole connect/dev/ttyACM0 250000")
+			
 			#result = subprocess.run(["G28"], capture_output=True)
 			return jsonify({'mensagem': 'Função executada com sucesso!'})
 		else:
