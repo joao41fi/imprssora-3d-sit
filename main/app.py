0fc272b051fa5d2ff508e5b9ec56dee13817d35e
joao41fi <97768350+joao41fi@users.noreply.github.com>
2023-03-02 23:01:57 +0000

    ko

--- main/app.py
+++ main/app.py
@@ -17,7 +17,7 @@ def Ligar_Impresora():
 		if request.form['checkbox_funcao'] == 'true':
 			print('primeira')
 			# Execute sua função aqui
-			enviar_comando_pronsole("connect/dev/ttyACM0 250000")
+			ligar_impressora()
 			
             
 			#result = subprocess.run(["G28"], capture_output=True)
--- main/scrpt.py
+++ main/scrpt.py
@@ -1,17 +1,28 @@
 import subprocess
 import time
 
-def enviar_comando_pronsole(comando):
+def ligar_impressora(porta='/dev/ttyACM0', velocidade=250000):
     # Abre o Pronsole em um processo separado
     process = subprocess.Popen(['pronsole'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
     # Aguarda alguns segundos para o Pronsole inicializar
     time.sleep(2)
 
-    # Envia o comando para o Pronsole
-    process.stdin.write(bytes(comando, 'utf-8') + b"\n")
+    # Envia o comando de conexão para o Pronsole
+    connect_cmd = f"connect {porta} {velocidade}\n"
+    process.stdin.write(connect_cmd.encode())
 
     # Lê a resposta do Pronsole
+    output = process.stdout.readline().decode()
+    print(output)
+
+    # Envia o comando G28 X e aguarda a resposta
+    process.stdin.write(b"G28 X\n")
+    output = process.stdout.readline().decode()
+    print(output)
+
+    # Envia o comando G28 Y e aguarda a resposta
+    process.stdin.write(b"G28 Y\n")
     output = process.stdout.readline().decode()
     print(output)
 
