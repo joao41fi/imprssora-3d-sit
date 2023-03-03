de8413cbaed70357fa8b812e7d7a852aa613179f
joao41fi <97768350+joao41fi@users.noreply.github.com>
2023-03-03 16:10:27 +0000

    Update imprime.py

--- imprime.py
+++ imprime.py
@@ -4,7 +4,7 @@ import time
 # Configure a porta serial com as configurações adequadas para a impressora 3D
 ser = serial.Serial('/dev/ttyACM0', 250000, timeout=1)
 # Abra o arquivo G-code e leia seu conteúdo
-with open('arquivo.gcode', 'r') as f:
+with open('/home/joao41/Desktop/imprssora-3d-sit/arquivo.gcode', 'r') as f:
     gcode = f.readlines()
 
 # Envie cada linha do arquivo G-code para a impressora 3D
