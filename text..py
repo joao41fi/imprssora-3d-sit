91aa18affc57beb206ed61b75d22e309d8a9a49a
joao41fi <97768350+joao41fi@users.noreply.github.com>
2023-03-03 15:52:24 +0000

    Update text..py

--- text..py
+++ text..py
@@ -1,5 +1,14 @@
-import pronsole 
 
-pr = pronsole.Pronsole('/dev/ttyACM0', 250000)  # substitua /dev/ttyACM0 pela porta serial que você está usando
-pr.connect()
-pr.send_now("G28")
+import serial
+import time
+
+# Configure a porta serial com as configurações adequadas para a impressora 3D
+ser = serial.Serial('/dev/ttyACM0', 250000, timeout=1)
+
+# Enviar alguns comandos para a impressora 3D
+ser.write(b"G28\n")  # Home all axes
+time.sleep(5)  # Espera 5 segundos
+ser.write(b"G1 X10 Y10 Z10 F500\n")  # Move to position (10, 10, 10) at speed 500
+
+# Fechar a porta serial
+ser.close()
