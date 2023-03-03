fd3425361eb9d01c39ae7799210f8d73566f3391
joao41fi <97768350+joao41fi@users.noreply.github.com>
2023-03-03 15:42:05 +0000

    Update text..py

--- text..py
+++ text..py
@@ -1,30 +1,5 @@
-import os
-import time
+import pronsole 
 
-# Inicia o Pronsole
-os.system("cd /")
-os.system("pronsole")
-
-# Aguarda alguns segundos para o Pronsole iniciar
-time.sleep(5)
-
-# Estabelece uma conexão com a impressora 3D
-os.system("echo connect ")
-
-# Aguarda a conexão ser estabelecida
-time.sleep(2)
-
-# Envia um comando G-code para a impressora 3D
-os.system("echo G28")
-
-# Aguarda a impressora 3D terminar de executar o comando
-time.sleep(2)
-
-# Envia outro comando G-code para a impressora 3D
-os.system("echo G1 X10 Y10 Z0.3 F1000 «")
-
-# Aguarda a impressora 3D terminar de executar o comando
-time.sleep(2)
-
-# Fecha o Pronsole
-os.system("echo quit")
+pr = pronsole.Pronsole('/dev/ttyACM0', 250000)  # substitua /dev/ttyACM0 pela porta serial que você está usando
+pr.connect()
+pr.send_now("G28")
