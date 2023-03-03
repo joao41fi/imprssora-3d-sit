3652264b539fbd6b46051769d5621ec563b998f7
joao41fi <97768350+joao41fi@users.noreply.github.com>
2023-03-03 15:57:19 +0000

    Update text..py

--- text..py
+++ text..py
@@ -6,6 +6,7 @@ import time
 ser = serial.Serial('/dev/ttyACM0', 250000, timeout=1)
 
 # Enviar alguns comandos para a impressora 3D
+time.sleep(5)
 ser.write(b"G28\n")  # Home all axes
 time.sleep(5)  # Espera 5 segundos
 ser.write(b"G1 X10 Y10 Z10 F500\n")  # Move to position (10, 10, 10) at speed 500
