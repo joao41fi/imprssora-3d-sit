d9fee6f162b82360f0e4acd6f92a0a923bc019d1
joao41fi <97768350+joao41fi@users.noreply.github.com>
2023-03-03 17:00:52 +0000

    Update imprime.py

--- imprime.py
+++ imprime.py
@@ -1,25 +1,32 @@
-
 import serial
 import time
 
 # Configure a porta serial com as configurações adequadas para a impressora 3D
 ser = serial.Serial('/dev/ttyACM0', 250000, timeout=1)
-# Abre o arquivo G-code para leitura
+# Abra o arquivo G-code e leia seu conteúdo
 with open('/home/joao41/Desktop/imprssora-3d-sit/arquivo.gcode', 'r') as f:
-    # Loop através de cada linha do arquivo G-code
-    for line in f:
-        # Remove espaços em branco e quebras de linha
-        line = line.strip()
-        
-        # Envia a linha para a impressora 3D
-        ser.write(line.encode('utf-8') + b'\n')
-        
-        # Espera a resposta "ok" da impressora 3D
-        while True:
-            response = ser.readline().decode('utf-8').strip()
-            if response == "ok":
-                break
-            time.sleep(0.1)
+    gcode = f.readlines()
+
+# Envie cada linha do arquivo G-code para a impressora 3D
+for line in gcode:
+    # Remova espaços em branco e quebras de linha
+    line = line.strip()
+
+    # Ignore linhas de comentário que começam com ";"
+    if line.startswith(';'):
+        continue
+
+    # Envie a linha para a impressora 3D e aguarde a resposta
+    ser.write(line.encode() + b'\n')
+    response = ser.readline()
+
+    # Imprima a resposta da impressora 3D
+   # print(response.decode().strip())
 
 # Fechar a porta serial
 ser.close()
+
+
+
+
+
