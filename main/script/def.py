52e6781c170ec0c05e64e5d8deea7b79ce04f8f4
joao41fi <97768350+joao41fi@users.noreply.github.com>
2023-03-04 00:23:57 +0000

    Create def.py

---
+++ main/script/def.py
@@ -0,0 +1,22 @@
+import serial
+import time
+
+def imprimir(ficheiro):
+
+    ser = serial.Serial('/dev/ttyACM0', 250000, timeout=1)
+    with open('/home/joao41/Desktop/imprssora-3d-sit/arquivo.gcode', 'r') as f:
+     gcode = f.readlines()
+
+
+    for line in gcode:
+    
+     line = line.strip()
+
+    
+     if line.startswith(';'):
+        continue
+     ser.write(line.encode() + b'\n')
+     response = ser.readline()
+
+    ser.close()
+   
