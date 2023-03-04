ccaa931d32c01b031815b7c017100e0caf425555
joao41fi <97768350+joao41fi@users.noreply.github.com>
2023-03-04 08:13:48 +0000

    Update def.py

--- main/script/def.py
+++ main/script/def.py
@@ -3,8 +3,8 @@ import time
 
 def imprimir(ficheiro):
 
-    ser = serial.Serial('/dev/ttyACM0', 250000, timeout=1)
-    with open('/home/joao41/Desktop/imprssora-3d-sit/arquivo.gcode', 'r') as f:
+    ser = serial.Serial('../dev/ttyACM0', 250000, timeout=1)
+    with open('../home/joao41/Desktop/imprssora-3d-sit/arquivo.gcode', 'r') as f:
      gcode = f.readlines()
 
 
