c79dc90aabd926964d3888e6e7ccd8aad8ea9721
joao41fi <97768350+joao41fi@users.noreply.github.com>
2023-03-05 19:22:46 +0000

    gi

--- comandos.py
+++ comandos.py
@@ -2,8 +2,4 @@ import subprocess
 import time
 
 # Executar um comando simples na console
-subprocess.run(["pronsole"])
-time.sleep(2)
-
-# Executar um comando mais complexo
-subprocess.run(["connect/dev/ttyACM0", "250000"])
+subprocess.run("pronsole -c Desktop/texte.cfg")
