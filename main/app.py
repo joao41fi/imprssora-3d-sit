3e93b8e7505efdfb96e1ecb3b54538afe0572315
joao41fi <97768350+joao41fi@users.noreply.github.com>
2023-03-05 12:48:15 +0000

    Update app.py

--- main/app.py
+++ main/app.py
@@ -76,7 +76,10 @@ def executar_funcao_3():
 def upload():
     file = request.files['file']
     caminho = '/home/joao41/Desktop/imprssora-3d-sit/main/uploads/' + file.filename
+
     atualizar_tabela("/home/joao41/Desktop/imprssora-3d-sit/main/ficheiro.db", "fichieros", "FICH ",caminho)
+
+
     file.save('/home/joao41/Desktop/imprssora-3d-sit/main/uploads/' + file.filename)
     return render_template('index.html')
 
