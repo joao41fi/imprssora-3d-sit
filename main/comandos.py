10f8ef53c9a7eff69dee8f0991315adfcceb82cc
joao41fi <97768350+joao41fi@users.noreply.github.com>
2023-03-05 20:49:34 +0000

    Update comandos.py

--- main/comandos.py
+++ main/comandos.py
@@ -2,4 +2,4 @@ import subprocess
 import time
 
 # Executar um comando simples na console
-subprocess.run(["pronsole","-c","Desktop/texte.cfg"])
+subprocess.run(["pronsole","-c","Desktop/imprssora-3d-sit/main/texte.cfg"])
