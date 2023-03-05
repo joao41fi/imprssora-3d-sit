876319967f662c8913c1712971c5ce5c92c63e37
joao41fi <97768350+joao41fi@users.noreply.github.com>
2023-03-05 20:54:44 +0000

    Update comandos.py

--- main/comandos.py
+++ main/comandos.py
@@ -1,5 +1,13 @@
 import subprocess
 import time
-
+import serial
 # Executar um comando simples na console
-subprocess.run(["pronsole","-c","Desktop/imprssora-3d-sit/texte.cfg"])
+#subprocess.run(["pronsole","-c","Desktop/imprssora-3d-sit/texte.cfg"])
+import os
+
+# Ejecutar un comando en CMD
+os.system('pronsole -c Desktop/imprssora-3d-sit/texte.cfg')
+
+
+resultado = os.popen('pronsole -c Desktop/imprssora-3d-sit/texte.cfg').read()
+print(resultado)
