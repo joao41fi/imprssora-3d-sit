0b3ee115ee2ed28e7f7643396b9ed2e77aeaeeb4
joao41fi <97768350+joao41fi@users.noreply.github.com>
2023-03-05 18:19:44 +0000

    Update comandos.py

--- comandos.py
+++ comandos.py
@@ -1,7 +1,9 @@
 import subprocess
+import time
 
 # Executar um comando simples na console
 subprocess.run(["pronsole"])
+time.sleep(2)
 
 # Executar um comando mais complexo
 subprocess.run(["connect/dev/ttyACM0", "250000"])
