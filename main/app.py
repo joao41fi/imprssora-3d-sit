6332a8da7db6db09bd2638ce1201f47445a767b8
joao41fi <97768350+joao41fi@users.noreply.github.com>
2023-03-06 19:03:50 +0000

    Update app.py

--- main/app.py
+++ main/app.py
@@ -86,7 +86,7 @@ def upload():
 
     atualizar_tabela("/home/joao41/Desktop/imprssora-3d-sit/main/ficheiro.db", "fichieros", "FICH ",caminhos)
     nova = 'load '+caminhos
-    velha = 'load '+ficheiro[0][0]
+    velha = 'load Desktop/imprssora-3d-sit/main/'+ficheiro[0][0]
     print(velha)
      # textar 
     
