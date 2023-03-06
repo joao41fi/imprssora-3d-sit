95f2872f0feb478cf9776019677d7a0d7419ead0
joao41fi <97768350+joao41fi@users.noreply.github.com>
2023-03-06 18:37:45 +0000

    Update app.py

--- main/app.py
+++ main/app.py
@@ -85,10 +85,10 @@ def upload():
     caminhos = '/home/joao41/Desktop/imprssora-3d-sit/main/uploads/' + file.filename
 
     atualizar_tabela("/home/joao41/Desktop/imprssora-3d-sit/main/ficheiro.db", "fichieros", "FICH ",caminhos)
-    # nova = 'load'+caminhos
-    velha = 'load'+str(ficheiro)
+    nova = 'load'+caminhos
+    velha = 'load'+ficheiro[0][0]
     print(velha)
-    #caminho(nova,velha) # textar 
+    caminho(nova,velha) # textar 
     
     file.save('/home/joao41/Desktop/imprssora-3d-sit/main/uploads/' + file.filename)
     return render_template('index.html')
