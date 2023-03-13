2d05b2a10bb5b47a37d46c5587e2f6c4999d98b1
joao41fi <97768350+joao41fi@users.noreply.github.com>
2023-03-13 12:56:12 +0000

    ok

--- textes/but-texte/app.py
+++ textes/but-texte/app.py
@@ -9,7 +9,8 @@ def index():
 @app.route('/button', methods=['POST'])
 def button():
     button_id = request.form['button_id']
-    if button_id == '1':
+    if button_id == 'E1':
+        print(button_id)
         print('Button 1 clicked')
     elif button_id == '2':
         print('Button 2 clicked')
@@ -17,7 +18,7 @@ def button():
         print('Button 3 clicked')
     elif button_id == '4':
         print('Button 4 clicked')
-    return ''
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
