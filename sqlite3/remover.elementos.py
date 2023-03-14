e5d6146cd9623c485f3e2a53424243029688bbff
joao41fi <97768350+joao41fi@users.noreply.github.com>
2023-03-14 14:40:45 +0000

    ok esta so por o historico

--- sqlite3/remover.elementos.py
+++ sqlite3/remover.elementos.py
@@ -1,2 +1,19 @@
 import sqlite3
+
 con = sqlite3.connect("sqlite3/tutorial.db")
+cur = con.cursor()
+
+# Definir o ID da linha que será excluída
+id_para_apagar = 4
+
+# Definir o comando SQL para excluir a linha
+comando_sql = f"DELETE FROM lista_impre WHERE rowid = {id_para_apagar};"
+
+# Executar o comando SQL
+cur.execute(comando_sql)
+
+# Salvar as mudanças no banco de dados
+con.commit()
+
+# Fechar a conexão com o banco de dados
+con.close()
